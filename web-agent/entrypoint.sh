7396353a1ee68cd9e06fcfecdea867c97710cfdc
Deepak Meena <135313425+dmeenaarmorcode@users.noreply.github.com>
2025-01-28 15:24:49 +0530

    Adding UID to appuser (#37)
    * Adding UID to appuser
    
    * addressing feedback

--- web-agent/entrypoint.sh
+++ web-agent/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Pass all arguments to the Python script and redirect output python
-/usr/src/venv/bin/python3 worker.py "$@" > /tmp/armorcode/log/console.log 2>&1
+/usr/src/venv/bin/python3 worker.py "$@" > /tmp/armorcode/console.log 2>&1
