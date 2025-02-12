2036f4cb6b139fa602bdecdbd3a40c7de9b5d0a2
dmeenaarmorcode <deepak.meena@armorcode.io>
2025-02-12 16:00:50 +0530

    Fixing timeout

--- web-agent/app/worker.py
+++ web-agent/app/worker.py
@@ -19,7 +19,7 @@ from urllib.parse import unquote
 import tempfile
 
 # Global variables
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 letters: str = string.ascii_letters
 rand_string: str = ''.join(secrets.choice(letters) for _ in range(10))
 
