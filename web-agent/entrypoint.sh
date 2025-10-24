1cf3f3cd1e38392a508e9a2913cc95ef37015259
dmeenaarmorcode <deepak.meena@armorcode.io>
2025-10-24 17:26:28 +0530

    Adding fix for latch timeout error

--- web-agent/entrypoint.sh
+++ web-agent/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Pass all arguments to the Python script and redirect output python
-/usr/src/venv/bin/python3 worker.py "$@" > /tmp/armorcode/console.log 2>&1
+/usr/src/venv/bin/python3 -W ignore worker.py "$@" > /dev/null 2> /tmp/armorcode/console.log
